ea5bb0638ae1605c3f55d7fb9db0bf0346167669
boberro <m.cyraniak@gmail.com>
2020-05-20 17:25:05 +0200

    build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+# mod_addons_path="./@folk_airsoft/addons"
+mod_addons_path="/f/SteamLibrary/steamapps/common/Arma 3/@folk_airsoft/addons"
+private_key_path="./.private\folk_airsoft_v1.0.0.biprivatekey"
+addons=(
+    folk_airsoft_core
+    folk_airsoft_hk416
+    folk_airsoft_p90
+    folk_airsoft_p99
+)
+# rm -r "$mod_addons_path/*"
+for addon in "${addons[@]}"; do
+    echo "${addon}"
+    pbo_path="${mod_addons_path}/${addon}.pbo"
+    armake build --force "${addon}" "${pbo_path}"
+    armake sign --force "${private_key_path}" "${pbo_path}"
+done
